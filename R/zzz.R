5a3879da872630edee7eadcebbdab604ab73b4cf
Martin Schlather <schlather@math.uni-mannheim.de>
2016-09-12 08:11:19 +0000

    version 0.3.3

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 }
 
 .onAttach <- function (lib, pkg) {
-   packageStartupMessage("This is RandomFieldsUtils Version: 0.3.2");
+   packageStartupMessage("This is RandomFieldsUtils Version: 0.3.3");
 }
 
 .onDetach <- function(lib){
